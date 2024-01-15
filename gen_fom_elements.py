23a17d12edcb4b05637b2bb9d3a8c7175821f362
Harlan Murphy <harlan3@hotmail.com>
2024-01-15 16:03:47 -0500

    Added generator for fom elements

---
+++ gen_fom_elements.py
@@ -0,0 +1,63 @@
+#! /usr/local/bin/python3
+import sys
+from xml.dom.minidom import parse
+
+# output xml file
+f = open("elementModel.xml", "w")
+
+# fom file as first arguement
+fomFile = sys.argv[1]
+
+datasource = open(fomFile)
+dom = parse(datasource)
+
+def getChildrenByName(node):
+    for child in node.childNodes:
+        if child.localName=='name':
+            yield child
+# objects
+objectList = []
+
+f.write("<?xml version='1.0' encoding='UTF-8'?>\n")
+f.write("<elementModel>\n")
+f.write("    <objects>\n")
+
+objectTags = dom.getElementsByTagName("objectClass") 
+for node in objectTags:
+    alist = getChildrenByName(node)
+    for a in alist:
+        nodeName = a.childNodes[0].nodeValue
+        #print(nodeName)
+        if (nodeName != "HLAobjectRoot"):
+            objectList.append(nodeName)
+
+objectList.sort()
+
+for objectName in objectList:
+    f.write("        <name>" + objectName + "</name>\n")
+
+f.write("    </objects>\n")
+
+# interactions
+interactionList = []
+
+f.write("    <interactions>\n")
+
+interactionTags = dom.getElementsByTagName("interactionClass") 
+for node in interactionTags:
+    alist = getChildrenByName(node)
+    for a in alist:
+        nodeName = a.childNodes[0].nodeValue
+        #print(nodeName)
+        if (nodeName != "HLAinteractionRoot"):
+            interactionList.append(nodeName)
+
+interactionList.sort()
+
+for interactionName in interactionList:
+    f.write("        <name>" + interactionName + "</name>\n")
+
+f.write("    </interactions>\n")
+
+f.write("</elementModel>\n")
+f.close()
