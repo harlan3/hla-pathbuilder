bfd0c6cf456f4deec78f8196f2068e205cf9578a
Harlan Murphy <harlan3@hotmail.com>
2025-02-26 17:57:00 -0500

    Delete test

--- test.cmd
+++
@@ -1,7 +0,0 @@
-echo off
-
-REM Add the required jars to classpath
-set CP=.\lib\commons-io-2.15.1.jar;.\lib\commons-lang3-3.0.jar;.\lib\commons-text-1.10.0.jar;.\lib\derby.jar;.\lib\derbyclient.jar;.\lib\derbyshared.jar;.\dist\hla_path_builder.jar;
-
-java -cp %CP% orbisoftware.hla_pathbuilder.MainApplication %*
-
